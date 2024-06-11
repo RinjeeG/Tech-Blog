bb861d120af7f426fa44f6552de62c8ba5ee3437
Rinjee <rinjigelu39@gmail.com>
2024-06-11 12:29:55 -0600

    revert changes

--- config/connection.js
+++ config/connection.js
@@ -5,17 +5,16 @@ let sequelize;
 
 if (process.env.DB_URL) {
     sequelize = new Sequelize(process.env.DB_URL);
-} else {
+  } else {
     sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.DB_HOST || 'localhost',
-            dialect: 'postgres',
-            port: process.env.DB_PORT || 5432
-        }
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres'
+      }
     );
-}
-
-module.exports = sequelize;
+  }
+  
+  module.exports = sequelize;
