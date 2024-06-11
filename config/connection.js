352de4751324f8401b16bcfc0820988a57934a21
Rinjee <rinjigelu39@gmail.com>
2024-06-11 12:19:34 -0600

    update .env

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,17 @@ let sequelize;
 
 if (process.env.DB_URL) {
     sequelize = new Sequelize(process.env.DB_URL);
-  } else {
+} else {
     sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres'
-      }
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'postgres',
+            port: process.env.DB_PORT || 5432
+        }
     );
-  }
-  
-  module.exports = sequelize;
+}
+
+module.exports = sequelize;
