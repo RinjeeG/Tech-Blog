b2e55f3aed5ecb1d12e4dfa0c4dea712f99f831a
Rinjee <rinjigelu39@gmail.com>
2024-06-17 21:31:53 -0600

    updated server.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,19 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const logoutButton = document.querySelector('#logout');
-  if (logoutButton) {
-    logoutButton.addEventListener('click', logout);
-  }
-});
-
-const logout = async () => {
+document.getElementById('logout').addEventListener('click', async () => {
   const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace('/');
+      document.location.replace('/login');
   } else {
-    alert('Failed to log out.');
+      alert('Failed to log out.');
   }
-};
+});
